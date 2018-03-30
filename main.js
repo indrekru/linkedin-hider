c5c1535f3173ff1e203d88176344d08aed48dcee
Indrek Ruubel <indrekruubel@gmail.com>
2018-03-30 16:52:09 +0300

    removed jquery

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ jsessionId = cleanupJsessionId(jsessionId);
 
 let loader, anonymousButton, fullyVisibleButton;
 
+let defaultClassNames = {
+    'loader' : 'm-t-3 m-b-2 text-center',
+    'anonymousButton' : 'button m-t-2',
+    'fullyVisibleButton' : 'button m-t-2'
+};
+
 let commandBodies = {
     'anonymous' : '------WebKitFormBoundary\r\nContent-Disposition: form-data; name="dataKey"\r\n\r\nprofileVisibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="el"\r\n\r\n#setting-profile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="settingsUrls"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="name"\r\n\r\nprofile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="locale"\r\n\r\nen_US\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="isNotCnDomain"\r\n\r\ntrue\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="headerData"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="path"\r\n\r\n/psettings/profile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="data"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="lixTests"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="pageTitle"\r\n\r\nProfile viewing\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="settingVisibility"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="device"\r\n\r\nDESKTOP\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="setting"\r\n\r\nprofile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="initialFetch"\r\n\r\ntrue\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="dataVal"\r\n\r\nDISCLOSE_FULL\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="hasSuccess"\r\n\r\nfalse\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="errors"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="discloseAsProfileViewer"\r\n\r\nHIDE\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="csrfToken"\r\n\r\n' + jsessionId + '\r\n------WebKitFormBoundary--\r\n',
     'full' : '------WebKitFormBoundary\r\nContent-Disposition: form-data; name="dataKey"\r\n\r\nprofileVisibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="el"\r\n\r\n#setting-profile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="settingsUrls"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="name"\r\n\r\nprofile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="dataVal"\r\n\r\nHIDE\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="locale"\r\n\r\nen_US\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="isNotCnDomain"\r\n\r\ntrue\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="headerData"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="path"\r\n\r\n/psettings/profile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="data"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="lixTests"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="pageTitle"\r\n\r\nProfile viewing\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="settingVisibility"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="device"\r\n\r\nDESKTOP\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="setting"\r\n\r\nprofile-visibility\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="initialFetch"\r\n\r\ntrue\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="hasSuccess"\r\n\r\nfalse\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="errors"\r\n\r\n[object Object]\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="discloseAsProfileViewer"\r\n\r\nDISCLOSE_FULL\r\n------WebKitFormBoundary\r\nContent-Disposition: form-data; name="csrfToken"\r\n\r\n' + jsessionId + '\r\n------WebKitFormBoundary--\r\n'
@@ -34,15 +40,25 @@ function cleanupJsessionId(jsessionId) {
 
 function runCommand(commandKey) {
     showLoading();
-    $.ajax({
-        method: 'POST',
-        url: "/psettings/profile-visibility",
-        contentType: 'multipart/form-data; boundary=----WebKitFormBoundary',
-        data: commandBodies[commandKey],
-        success: function(result){
-            location.reload();
-        }
+    http('POST', '/psettings/profile-visibility', commandBodies[commandKey], {
+        'Content-Type' : 'multipart/form-data; boundary=----WebKitFormBoundary',
+        'x-requested-with' : 'XMLHttpRequest'
+    }, function(rawResponse) {
+        location.reload();
     });
+}
+
+function http(method, url, requestBody, headers, callback){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open(method, url, true); // true for asynchronous
+    for (headerKey in headers) {
+        xmlHttp.setRequestHeader(headerKey, headers[headerKey]);   
+    }
+    xmlHttp.send(requestBody);
 }
 
 function fullyAnonymous(){
@@ -53,45 +69,44 @@ function fullyVisible() {
     runCommand('full');
 }
 
-function defer(method) {
-    if (window.$) {
-        method();
-    } else {
-        setTimeout(function() { defer(method) }, 50);
-    }
-}
-
 function showLoading() {
-    $(loader).removeClass('hide');
-    $(anonymousButton).addClass('hide');
-    $(fullyVisibleButton).addClass('hide');
+    loader.className = defaultClassNames['loader'];
+    anonymousButton.className = defaultClassNames['anonymousButton'] + ' hide';
+    fullyVisibleButton.className = defaultClassNames['fullyVisibleButton'] + ' hide';
 }
 
 function showAnonymousButton() {
-    $(loader).addClass('hide');
-    $(anonymousButton).removeClass('hide');
-    $(fullyVisibleButton).addClass('hide');
+    loader.className = defaultClassNames['loader'] + ' hide';
+    anonymousButton.className = defaultClassNames['anonymousButton'];
+    fullyVisibleButton.className = defaultClassNames['fullyVisibleButton'] + ' hide';
 }
 
 function showVisibleButton() {
-    $(loader).addClass('hide');
-    $(anonymousButton).addClass('hide');
-    $(fullyVisibleButton).removeClass('hide');
+    loader.className = defaultClassNames['loader'] + ' hide';
+    anonymousButton.className = defaultClassNames['anonymousButton'] + ' hide';
+    fullyVisibleButton.className = defaultClassNames['fullyVisibleButton'];
 }
 
-function isVisible() {    
-    $.ajax({
-        method: 'GET',
-        url: '/psettings/profile-visibility?asJson=true',
-        success: function(result){
-            let visibility = result.map.data.profileVisibility;
-            if (visibility === 'DISCLOSE_FULL') {
-                // Visible
-                showAnonymousButton();
-            } else {
-                // Hidden
-                showVisibleButton();
-            }
+function isVisible(callback) {
+    http('GET', '/psettings/profile-visibility?asJson=true', null, {}, function(rawResponse) {
+        let result = JSON.parse(rawResponse);
+        let visibility = result.map.data.profileVisibility;
+        if (visibility === 'DISCLOSE_FULL') {
+            // Visible
+            callback(true);
+        } else {
+            // Hidden
+            callback(false);
+        }
+    });
+}
+
+function init() {
+    isVisible(function(isVisible) {
+        if (isVisible) {
+            showAnonymousButton();
+        } else {
+            showVisibleButton();
         }
     });
 }
@@ -115,19 +130,19 @@ container.appendChild(hider);
 
 loader = document.createElement('p');
 loader.innerHTML = 'Loading...'
-loader.className = 'm-t-3 m-b-2 text-center';
+loader.className = defaultClassNames['loader'];
 container.appendChild(loader);
 
 anonymousButton = document.createElement('button');
 anonymousButton.innerHTML = 'Make me anonymous';
 anonymousButton.onclick = fullyAnonymous;
-anonymousButton.className = 'button m-t-2 hide';
+anonymousButton.className = defaultClassNames['anonymousButton'] + ' hide';
 container.appendChild(anonymousButton);
 
 fullyVisibleButton = document.createElement('button');
 fullyVisibleButton.innerHTML = 'Make me visible';
 fullyVisibleButton.onclick = fullyVisible;
-fullyVisibleButton.className = 'button m-t-2 hide';
+fullyVisibleButton.className = defaultClassNames['fullyVisibleButton'] + ' hide';
 container.appendChild(fullyVisibleButton);
 
 document.body.appendChild(container);
@@ -137,7 +152,5 @@ document.body.appendChild(container);
 *   Main
 */
 
-defer(function () {
-    isVisible();
-    console.log('LinkedIn Hider loaded');
-});
+init();
+console.log('LinkedIn Hider loaded');
